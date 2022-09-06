8573cce53707bf8f745e63c174151626cdf66cbf
Lucas Carioca <lucascarioca97@gmail.com>
2022-09-06 19:40:51 -0300

    inital commit

---
+++ graph.js
@@ -0,0 +1,87 @@
+class Graph {
+  constructor(nVertex) {
+    this.nVertex = nVertex
+    this.adjList = new Map()
+    this.color = new Map()
+    this.distance = new Map()
+    this.parent = new Map()
+  }
+
+  // Adiciona vértice v com lista de adjacência vazia
+  addVertex(v) {
+    this.adjList.set(v, [])
+    this.color.set(v, "white")
+    this.distance.set(v, NaN)
+    this.parent.set(v, null)
+  }
+
+  // Adiciona aresta de v para w
+  addEdge(v, w) {
+    this.adjList.get(v).push(w)
+    this.adjList.get(w).push(v) // Adiciona mesma aresta em w, já que é um grafo sem direção
+  }
+
+  printGraph() {
+    let getKeys = this.adjList.keys()
+
+    for (let i of getKeys) {
+      let getValues = this.adjList.get(i)
+      let conc = ""
+
+      for (let j of getValues) {
+        conc += j + " "
+      }
+      console.log(`${i} -> ${conc}`)
+    }
+  }
+
+  printColorDistanceParent() {
+    let getKeys = this.color.keys()
+    for (let i of getKeys) {
+      let vertColor = this.color.get(i)
+      let vertDist = this.distance.get(i)
+      let vertParent = this.parent.get(i)
+      console.log(`${i}: parent=${vertParent} distance=${vertDist} color=${vertColor}`)
+    }
+  }
+
+  bfs(s) {
+
+    this.color.set(s, "gray")
+    this.distance.set(s, 0)
+    this.parent.set(s, null)
+
+    let queue = []
+    queue.push(s)
+
+    while (queue.length > 0) {
+      let u = queue.splice(0, 1)[0]
+      let getList = this.adjList.get(u)
+
+      for (let i in getList) {
+        let neighbor = getList[i]
+
+        if (this.color.get(neighbor) === "white") {
+          this.color.set(neighbor, "gray")
+          this.distance.set(neighbor, this.distance.get(u) + 1)
+          this.parent.set(neighbor, u)
+          queue.push(neighbor)
+        }
+      }
+      this.color.set(u, "black")
+    }
+  }
+
+  printBestPath(s, f) {
+    if (f == s) {
+      console.log(s)
+    } else if (this.parent.get(f) == null) {
+      console.log("Nenhum caminho existente")
+    } else {
+      this.printBestPath(s, this.parent.get(f))
+      console.log(f)
+    }
+  }
+}
+
+module.exports = Graph;
---
+++ index.js
@@ -0,0 +1,64 @@
+const Graph = require('./graph')
+
+let g = new Graph(32)
+// representação dos vértices em printPacman.png
+let vertices = [
+  'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12', 'A13',
+  'B1', /*--------------------------*/'B7', /*-----------------------------*/'B13',
+  'C1', /*--------------------------*/'C7', /*-----------------------------*/'C13',
+  'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'D11', 'D12', 'D13',
+]
+
+for (let i = 0; i < vertices.length; i++) {
+  g.addVertex(vertices[i])
+}
+// Adiciona Arestas
+g.addEdge('A1', 'A2');
+g.addEdge('A2', 'A3');
+g.addEdge('A3', 'A4');
+g.addEdge('A4', 'A5');
+g.addEdge('A5', 'A6');
+g.addEdge('A6', 'A7');
+g.addEdge('A7', 'A8');
+g.addEdge('A8', 'A9');
+g.addEdge('A9', 'A10');
+g.addEdge('A10', 'A11');
+g.addEdge('A11', 'A12');
+g.addEdge('A12', 'A13');
+g.addEdge('A13', 'B13');
+g.addEdge('B13', 'C13');
+g.addEdge('C13', 'D13');
+g.addEdge('D13', 'D12');
+g.addEdge('D12', 'D11');
+g.addEdge('D11', 'D10');
+g.addEdge('D10', 'D9');
+g.addEdge('D9', 'D8');
+g.addEdge('D8', 'D7');
+g.addEdge('D7', 'C7');
+g.addEdge('C7', 'B7');
+g.addEdge('B7', 'A7');
+g.addEdge('D7', 'D6');
+g.addEdge('D6', 'D5');
+g.addEdge('D5', 'D4');
+g.addEdge('D4', 'D3');
+g.addEdge('D3', 'D2');
+g.addEdge('D2', 'D1');
+g.addEdge('D1', 'C1');
+g.addEdge('C1', 'B1');
+g.addEdge('B1', 'A1');
+
+// print Array de vértices
+console.log(vertices)
+console.log("\n\n\n")
+// print lista de adjacência
+console.log("Lista de Adjacência")
+g.printGraph();
+console.log("\n\n\n")
+// Realiza Busca em Largura
+g.bfs('A13')
+console.log("Resultado Busca em Largura")
+g.printColorDistanceParent()
+console.log("\n\n\n")
+// Busca melhor caminho
+console.log("Melhor caminho")
+g.printBestPath('A13', 'D2')
